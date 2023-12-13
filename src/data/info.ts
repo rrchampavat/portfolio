7b230924baf2dd3875e307d099fa549dc878c43f
Rutvikraj <work.rrchampavat@gmail.com>
2023-12-13 13:55:39 +0530

    Initial commit

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,97 +1,69 @@
 export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
-  jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  name: "Rutvikrajsinh Champavat",
+  jobDescription: "Software Engineer",
+  about: `👋 Welcome to my corner of the internet. 
+  I'm a chill software engineer with two years under my belt, rocking JavaScript, TypeScript, 
+  ReactJS, NextJS, Redux, HTML5, CSS3, NodeJS, ExpressJS, MongoDB, MySQL, PostgreSQL, and GitHub. 
+  I love creating smooth user experiences and building robust back-end solutions. 
+  Feel free to check out my work – I'm all about cool projects and good vibes. 
+  Let's chat and see how I can bring some coding magic to your team! 🚀✨.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "Stack Summation",
+      location: "Ahmedabad, Gujarat, India",
+      startDate: "Dev 2021",
+      endDate: "Sept 2022",
+      description: [
+        "✓ Responsible for the development and implementation of web applications based on client requirements.",
+        "✓ Enhance and improve the existing system.",
+        "✓ Created responsive websites from scratch.",
+        "✓ Support and development of new functionality for already existing projects.",
+      ],
+    },
+
+    {
+      name: "Torinit Technologies",
+      location: "Pune, Maharashtra, India",
+      startDate: "Oct 2022",
       endDate: "Present",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "✓ Wrote performant, readable code for various applications",
+        "✓ Debugged issues and refactored code for various features",
+        "✓ Built responsive web pages",
+        "✓ Communicated with Department Employees for Requirements",
+        "✓ Developed software currently in production",
+        "✓ Making changes (fixing/upgrading) to existing applications",
+        "✓ Came up with solutions for existing issues",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2019",
-      endDate: "2022",
-      description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-      ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
+      name: "A D Patel Institute of Technology",
+      location: "Vidyangar, Gujarat, India",
+      startDate: "2017",
+      endDate: "2021",
+      description: ["Bachelors in Engineering", "Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
     github: "https://github.com/uzzii-21",
-    email: "mailto:uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    email: "rrchampavat1110@gmail.com",
+    linkedin: "https://www.linkedin.com/in/rutvikrajsinh-champavat-4b483b168/",
   },
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Media Player",
       isFeatured: true,
-      thumbnail: "/assets/images/astro-portfolio.png",
-      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
-      liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+      thumbnail: "/assets/images/media-player-home.png",
+      githubUrl: "https://github.com/rrchampavat/media-player-1",
+      liveUrl: "https://media-player-1.vercel.app/",
     },
   ],
 };
